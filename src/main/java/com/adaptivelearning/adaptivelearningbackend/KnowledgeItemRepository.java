dbd0181de9e99d885127462eb926f01592d43717
PracticingMaggotry <soriano.357912@munoz.sti.edu.ph>
2026-06-23 19:19:58 +0800

    Fixes Codes

--- src/main/java/com/adaptivelearning/adaptivelearningbackend/KnowledgeItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.adaptivelearning.adaptivelearningbackend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface KnowledgeItemRepository extends JpaRepository<KnowledgeItem, Long> {
-    List<KnowledgeItem> findByTopicIgnoreCaseOrderByCreatedAtDesc(String topic);
-    List<KnowledgeItem> findByTopicIgnoreCaseAndCreatedByOrderByCreatedAtDesc(String topic, String createdBy);
-}
